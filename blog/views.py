5583d9388d5ce271e284118263b13244ea2de3cd
raierin <erinrai5874@gmail.com>
2020-08-17 14:20:14 +0545

    user management system in progress

--- blog/views.py
+++ blog/views.py
@@ -25,22 +25,18 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
 
     form = CommentForm()
-
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = Comment(
-                author = form.cleaned_data['author'],
-                body = form.cleaned_data['body'],
-                post = post
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
             )
             comment.save()
-    comments = Comment.Objects.filter(post=post)
-    context = {
-        "post": post,
-        "comments": comments,
-    }
 
+    context = {"post": post, "comments": comments, "form": form}
     return render(request, "blog_detail.html", context)
